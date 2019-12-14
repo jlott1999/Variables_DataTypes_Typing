021ea8f0a1e972044132ab0171a8c72c6a561231
Jacob Lott <jlott3@cnm.edu>
2019-12-13 19:37:09 -0700

    Updated

---
+++ Second File.js
@@ -0,0 +1,12 @@
+/*
+* Variables, Data Types, and Typing
+ */
+let obj = {
+    key1: "Value 1",
+    ket2: 3,
+    obj2: {
+        obj2Key1 : "value of inner object";
+},
+};
+
+console.log(obj.obj2.obj2Key1);
