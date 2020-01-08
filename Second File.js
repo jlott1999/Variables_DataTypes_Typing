1c8429011fe00367f627ae98527d3c74d06a2a4e
Jacob Lott <jlott3@cnm.edu>
2020-01-08 09:51:29 -0700

    Updated

--- Second File.js
+++ Second File.js
@@ -5,8 +5,8 @@ let obj = {
     key1: "Value 1",
     ket2: 3,
     obj2: {
-        obj2Key1 : "value of inner object";
-},
+        obj2Key1 : `value of inner object`
+}
 };
 
 console.log(obj.obj2.obj2Key1);
